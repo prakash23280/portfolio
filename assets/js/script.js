cc0ff9b8dccd29988651e920200d5331a2ad62ee
Prakash <prakash23280@gmail.com>
2026-07-17 14:50:48 +0530

    Reorganize assets and add social icons

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const roles = [
+    "Junior DevOps Engineer",
+    "AWS Enthusiast",
+    "Linux Administrator",
+    "Docker & Kubernetes Learner",
+    "Cloud Engineer"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.getElementById("typing");
+
+function type() {
+
+    const current = roles[roleIndex];
+
+    if (!deleting) {
+        typing.textContent = current.substring(0, charIndex++);
+    } else {
+        typing.textContent = current.substring(0, charIndex--);
+    }
+
+    let speed = deleting ? 50 : 100;
+
+    if (!deleting && charIndex === current.length + 1) {
+        deleting = true;
+        speed = 1800;
+    }
+
+    if (deleting && charIndex === 0) {
+        deleting = false;
+        roleIndex = (roleIndex + 1) % roles.length;
+    }
+
+    setTimeout(type, speed);
+}
+
+type();
