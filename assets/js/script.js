f922fc56a5cc0fdb4d8546455a0bdb52724e5d69
prakash23280 <prakash23280@gmail.com>
2026-07-19 00:16:41 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,9 @@ function type() {
 }
 
 type();
+const menu = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("nav-links");
+
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
